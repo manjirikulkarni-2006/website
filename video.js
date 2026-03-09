bae648e1662c47826f53138c013195b38310d3d1
manjirikulkarni-2006 <manjirikulkarni5157@gmail.com>
2026-03-09 22:40:54 +0530

    Enhance video playback with background music
    Added background music functionality and updated event handling for video playback.

--- video.js
+++ video.js
@@ -1,22 +1,33 @@
+let box1 = document.getElementById("box1");
+let video1 = document.getElementById("video1");
+let text = document.getElementById("text");
 
-let box1=document.getElementById("box1");
-let video1=document.getElementById("video1");
-let text=document.getElementById("text");
+let gift = document.getElementById("gift");
+let video3 = document.getElementById("video3");
 
-let gift=document.getElementById("gift");
-let video3=document.getElementById("video3");
+let music = document.getElementById("bgmusic"); // background music
 
-box1.addEventListener("click",function(){
-    text.style.display="none";
-    video1.style.display="block";
+box1.addEventListener("click", function () {
+
+    text.style.display = "none";
+
+    video1.style.display = "block";
     video1.play();
-    gift.style.display="block"
+
+    music.play(); // start music
+
+    // show gift button after video finishes
+    video1.onended = function () {
+        gift.style.display = "block";
+    };
 
 });
 
-gift.addEventListener("click",function(){
-    video1.style.display="none";
-    video3.style.display="block";
+gift.addEventListener("click", function () {
+
+    video1.style.display = "none";
+
+    video3.style.display = "block";
     video3.play();
-    
-})
+
+});

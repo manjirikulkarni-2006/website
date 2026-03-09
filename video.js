7206d9fe6927df22775ac6c4680758d6707d7d15
manjirikulkarni-2006 <manjirikulkarni5157@gmail.com>
2026-03-09 22:45:44 +0530

    Update video.js

--- video.js
+++ video.js
@@ -1,38 +1,22 @@
-let box1 = document.getElementById("box1");
-let video1 = document.getElementById("video1");
-let text = document.getElementById("text");
 
-let gift = document.getElementById("gift");
-let video3 = document.getElementById("video3");
+let box1=document.getElementById("box1");
+let video1=document.getElementById("video1");
+let text=document.getElementById("text");
 
-let music = document.getElementById("bgmusic");
+let gift=document.getElementById("gift");
+let video3=document.getElementById("video3");
 
-box1.addEventListener("click", function () {
-
-    text.style.display = "none";
-
-    video1.style.display = "block";
+box1.addEventListener("click",function(){
+    text.style.display="none";
+    video1.style.display="block";
     video1.play();
-
-    music.play();
-
-});
-
-/* Show gift button when first video ends */
-
-video1.addEventListener("ended", function () {
-
-    gift.style.display = "block";
+    gift.style.display="block"
 
 });
 
-/* Gift button click */
-
-gift.addEventListener("click", function () {
-
-    video1.style.display = "none";
-
-    video3.style.display = "block";
+gift.addEventListener("click",function(){
+    video1.style.display="none";
+    video3.style.display="block";
     video3.play();
-
-});
+    
+})
